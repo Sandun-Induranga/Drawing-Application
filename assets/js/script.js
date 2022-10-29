90e6b446afe606fe126d559bba58b3abe7aced6b
Sandun-Induranga <sandutharu40@gmail.com>
2022-10-29 11:59:53 +0530

    Content Updated

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+let painting = false;
+
+$("canvas").on("mousedown", function () {
+    alert("done");
+});
+
+$("canvas").on("mouseup", function () {
+    alert("done");
+});
+
+$("canvas").on("mousemove", function () {
+    alert("done");
+});
