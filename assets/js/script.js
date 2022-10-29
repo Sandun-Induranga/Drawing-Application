fa375d7825d09ca28e271e66a33b44d6ae6002f6
Sandun-Induranga <sandutharu40@gmail.com>
2022-10-29 21:14:43 +0530

    Content Updated

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,37 +1,49 @@
-// $(window).on('load', function () {
-    let drawing = false;
-    const canvas = document.querySelector("canvas");
-    const line = canvas.getContext("2d");
-    canvas.height = $("body").height();
-    canvas.width = $("body").width();
+let drawing = false;
+const canvas = document.querySelector("canvas");
+const line = canvas.getContext("2d");
+canvas.height = $("body").height();
+canvas.width = $("body").width();
+let downX = 0;
+let downY = 0;
 
-    $(this).on("mousedown", function (e) {
-        drawing = true;
-        draw(e);
-    });
+$(this).on("mousedown", function (e) {
+    drawing = true;
+    downX = e.pageX;
+    downY = e.pageY;
+    draw(e);
+});
 
-    $(this).on("mouseup", function () {
-        drawing = false;
-        line.beginPath();
-    });
+$(this).on("mouseup", function (e) {
+    // drawing = false;
+    // line.beginPath();
+    line.rect(downX, downY, e.pageX-downX, e.pageY-downY);
+    line.stroke();
+    line.beginPath();
+    line.rect(downX, downY, e.pageX-downX, e.pageY-downY);
+});
 
-    $(this).on("mousemove", function (e) {
-        draw(e);
-    });
+$(this).on("mousemove", function (e) {
+    draw(e);
+});
 
-    function draw(e) {
-        if (!drawing) return;
-        line.lineWidth = 10;
-        line.lineCap = "round";
-        line.strokeStyle = $("#color").val();
+function draw(e) {
+    if (!drawing) return;
+    line.lineWidth = 10;
+    line.lineCap = "round";
+    line.strokeStyle = $("#color").val();
 
-        line.lineTo(e.pageX, e.pageY);
-        line.stroke();
-        line.beginPath();
-        line.lineTo(e.pageX, e.pageY);
-    }
+    // line.lineTo(e.pageX, e.pageY);
+    // line.rect(e.pageX, e.pageY, e.pageX-downX, e.pageY-downY);
+    // line.stroke();
+    // line.beginPath();
+    // line.rect(e.pageX, e.pageY, e.pageX-downX, e.pageY-downY);
+    // line.lineTo(e.pageX, e.pageY);
+}
 
-    // $("#canvas").on("click", function () {
-    //     line.fillStyle = $("#color").value();
-    // })
-// });
+// $("#canvas").on("click", function () {
+//     line.fillStyle = $("#color").value();
+// })
+
+$("#square").on("click", function () {
+
+})
